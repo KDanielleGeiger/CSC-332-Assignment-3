b489e02116d089254e8d57fe85e02b33b57b911e
KDanielleGeiger <42385182+KDanielleGeiger@users.noreply.github.com>
2018-10-22 11:44:25 -0500

    Added comments and @cache

--- PA-3-Source.py
+++ PA-3-Source.py
@@ -2,6 +2,7 @@ import time
 from tkinter import *
 from functools import partial
 
+##  Decorator that caches results for the fibDynamic function
 def cache(fn):
     CACHE = {}
 
@@ -15,10 +16,12 @@ def cache(fn):
 
     return wrapper
 
+##  Creates the UI
 def main():
     window = Tk()
     window.title("Fibonacci Series")
 
+    ##  Takes user input
     entry = Entry(window)
     entry.grid(row=0, column=0, padx=(15,5), pady=(15,0))
     entry.insert(0, 'Enter n ≥ 0')
@@ -26,31 +29,38 @@ def main():
     entry.bind('<FocusOut>', partial(onFocusOut, entry))
     entry.config(fg='light grey')
 
+    ##  Displays the result F(n)
     Label(window, text='F(n): ').grid(row=1,column=0,sticky='W',pady=(15,0),padx=(10,0))
     valueStr = StringVar()
     valueStr.set('')
     lbl1 = Label(window, textvariable = valueStr, fg='blue').grid(row=1,column=0,sticky='E',pady=(15,0))
-    
+
+    ##  Displays the result time
     Label(window, text='Time: ').grid(row=2,column=0,sticky='W',padx=(10,0),pady=(0,15))
     timeStr = StringVar()
     timeStr.set('')
     lbl2 = Label(window, textvariable = timeStr, fg='blue').grid(row=2,column=0,sticky='E',pady=(0,15))
 
+    ##  User chooses to use the recursive algorithm or the DP algorithm 
     Button(window, text='Recursive', command=partial(display1, entry, valueStr, timeStr)).grid(row=0,column=1,pady=(15,0))
     Button(window, text='Dynamic', command=partial(display2, entry, valueStr, timeStr)).grid(row=0,column=2,pady=(15,0),padx=(5,15))
-    
+
+    ##  Continue to display UI and wait for user input
     window.mainloop()
 
+##  Removes placeholder text in entry box when user focuses in
 def onFocusIn(entry, e):
     if entry.get() == 'Enter n ≥ 0':
         entry.delete(0, END)
         entry.config(fg='black')
 
+##  Replaces placeholder text if user focuses out and no input was given
 def onFocusOut(entry, e):
     if entry.get() == '':
         entry.insert(0, 'Enter n ≥ 0')
         entry.config(fg = 'light grey')
 
+##  Checks that n is a valid value and calls fibRecursive
 def display1(entry, valueStr, timeStr):
     try:
         n = int(Entry.get(entry))
@@ -65,6 +75,7 @@ def display1(entry, valueStr, timeStr):
     except:
         displayResults('Invalid Input', 'Invalid Input', valueStr, timeStr)
 
+##  Checks that n is a valid value and calls fibDynamic
 def display2(entry, valueStr, timeStr):
     try:
         n = int(Entry.get(entry))
@@ -79,6 +90,7 @@ def display2(entry, valueStr, timeStr):
     except:
         displayResults('Invalid Input', 'Invalid Input', valueStr, timeStr)
 
+##  Helper function to format results and display them on the UI
 def displayResults(value, time, valueStr, timeStr):
     if isinstance(value, str):
         timeSpent = time
@@ -88,6 +100,7 @@ def displayResults(value, time, valueStr, timeStr):
     valueStr.set(value)
     timeStr.set(timeSpent)
 
+##  The recursive Fibonacci algorithm
 def fibRecursive(n):
     if n == 0:
         return 1
@@ -96,18 +109,16 @@ def fibRecursive(n):
     else:
         return (fibRecursive(n-1) + fibRecursive(n-2))
 
+##  The dynamic Fibonacci algorithm
+##  Uses cache decorator to cache results and improve performance
+@cache
 def fibDynamic(n):
     a = b = 1
-    if n == 0:
-        return 1
-    else:
-        for x in range(0, n-1):
-            temp = a + b
-            a = b
-            b = temp
+    for x in range(0, n-1):
+        temp = a + b
+        a = b
+        b = temp
     return b
-
-fibDynamic = cache(fibDynamic)
 
 if __name__ == "__main__":
     main()
