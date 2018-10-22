304afe0d200bb6197060a9e02a669cd0bc499eb0
KDanielleGeiger <42385182+KDanielleGeiger@users.noreply.github.com>
2018-10-21 21:27:38 -0500

    Added UI

--- PA-3-Source.py
+++ PA-3-Source.py
@@ -1,10 +1,56 @@
+import time
 from tkinter import *
+from functools import partial
 
 def main():
     window = Tk()
     window.title("Fibonacci Series")
+
+    entry = Entry(window)
+    entry.grid(row=0, column=0, padx=(15,5))
+    entry.insert(0, 'Enter n ≥ 0')
+    entry.bind('<FocusIn>', partial(onFocusIn, entry))
+    entry.bind('<FocusOut>', partial(onFocusOut, entry))
+    entry.config(fg='light grey')
+
+    Button(window, text='Recursive', command=partial(display1, entry)).grid(row=0,column=1)
+    Button(window, text='Dynamic', command=partial(display2, entry)).grid(row=0,column=2,padx=(5,15))
     
     window.mainloop()
+
+def onFocusIn(entry, e):
+    if entry.get() == 'Enter n ≥ 0':
+        entry.delete(0, END)
+        entry.config(fg='black')
+
+def onFocusOut(entry, e):
+    if entry.get() == '':
+        entry.insert(0, 'Enter n ≥ 0')
+        entry.config(fg = 'light grey')
+
+def display1(entry):
+    try:
+        n = int(Entry.get(entry))
+        if n >= 0:
+            startTime = time.perf_counter()
+            print("Call recursive function")
+            elapsedTime = round(((time.perf_counter() - startTime) * 1000), 6)
+        else:
+            print("Not Good")
+    except:
+        print("Not Good")
+
+def display2(entry):
+    try:
+        n = int(Entry.get(entry))
+        if n >= 0:
+            startTime = time.perf_counter()
+            print("Call dynamic function")
+            elapsedTime = round(((time.perf_counter() - startTime) * 1000), 6)
+        else:
+            print("Not Good")
+    except:
+        print("Not Good")
 
 def fibRecursive(n):
     if n == 0:
